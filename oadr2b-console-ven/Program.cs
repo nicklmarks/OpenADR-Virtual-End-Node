9810e80f0a0d417e120e0a325d24f7cfaa8b4abc
Nicholas Marks <nickloop@gmail.com>
2017-10-26 18:36:13 -0700

    Create Program.cs

---
+++ oadr2b-console-ven/Program.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace oadr2b_ven
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+#if (!DEBUG)
+            frmSplash splash = new frmSplash();
+
+            DialogResult result = splash.ShowDialog();
+
+            if (result != System.Windows.Forms.DialogResult.OK)
+                return;
+#endif
+//            Application.Run(new frmMain());
+
+            Application.Run(new consoleMain());
+
+        }
+    }
+}
+
+
